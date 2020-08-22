a4c52fceb8587ff60f391d98c2cc8d2f85c4067a
wrkzdev <40448869+wrkzdev@users.noreply.github.com>
2020-08-22 10:29:58 +0700

    Update CryptoNote.ts

--- src/CryptoNote.ts
+++ src/CryptoNote.ts
@@ -689,7 +689,7 @@ export class CryptoNote {
         try {
             await tx.generateTxProofOfWork(diff);
         } catch (error) {
-            throw new Error('Can not generateTxProofOfWork()');
+            throw new Error('Can not generateTxProofOfWork() for diff: ' + diff);
         }
 
         if (tx.extra.length > (this.config.maximumExtraSize || Config.maximumExtraSize)) {
