8c65269f13ab23ac5039e4e327d241fdfd9bc331
wrkzdev <40448869+wrkzdev@users.noreply.github.com>
2020-08-22 10:47:54 +0700

    Update Transaction.ts

--- src/Transaction.ts
+++ src/Transaction.ts
@@ -489,32 +489,32 @@ export class Transaction {
             throw new Error('Transaction is read-only');
         }
 
-        let nonceTag = new ExtraTag.ExtraPowNonce(BigInteger(0));
-
-        let result: ExtraTag.IExtraTag[] = [];
-
-        for (const tag of this.m_extra) {
-            if (tag.tag !== nonceTag.tag) {
-                result.push(tag);
-            }
-        }
-
-        this.m_extra = result;
-        this.m_extra.push(nonceTag);
-
-        const prefix = this.prefix;
-
-        /* Find the pow nonce tag and nonce hole */
-        const tagOffset = prefix.indexOf("040000000000000000");
-
-        /* Then add 2 to skip the tag. */
-        const nonceOffset = tagOffset + 2;
-
-        /* Actual offset is half the nonce offset, since this is hex, but it
-         * will be deserialized into bytes taking up half the space */
-        const unserializedOffset = nonceOffset / 2;
-
         try {
+            let nonceTag = new ExtraTag.ExtraPowNonce(BigInteger(0));
+
+            let result: ExtraTag.IExtraTag[] = [];
+
+            for (const tag of this.m_extra) {
+                if (tag.tag !== nonceTag.tag) {
+                    result.push(tag);
+                }
+            }
+
+            this.m_extra = result;
+            this.m_extra.push(nonceTag);
+
+            const prefix = this.prefix;
+
+            /* Find the pow nonce tag and nonce hole */
+            const tagOffset = prefix.indexOf("040000000000000000");
+
+            /* Then add 2 to skip the tag. */
+            const nonceOffset = tagOffset + 2;
+
+            /* Actual offset is half the nonce offset, since this is hex, but it
+             * will be deserialized into bytes taking up half the space */
+            const unserializedOffset = nonceOffset / 2;
+
             const nonce = TurtleCoinCrypto.generateTransactionPow(prefix, unserializedOffset, diff);
             nonceTag = new ExtraTag.ExtraPowNonce(BigInteger(nonce));
 
